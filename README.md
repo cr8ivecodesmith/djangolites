25f1637e2af6353f552e5018c261c7b213cd6b47
Matt Lebrun <lebrun.matt@gmail.com>
2014-07-25 13:28:30 +0800

    CH01 Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Lightweight Django Chapter Excercises
+=====================================
+
+## Chapter 1 - World's Smallest Django Project
+
+All you need is a single file with the most minimum settings, a view, and a url pattern.
---
+++ ch01/hello.py
@@ -0,0 +1,32 @@
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+##### SETTINGS
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='supersecretkeykeepitsafe',
+    ROOT_URLCONF=__name__,
+)
+##### END SETTINGS
+
+
+##### VIEWS
+def index(request):
+    return HttpResponse('Hello World')
+##### END VIEWS
+
+
+##### URLS
+urlpatterns = (
+    url(r'^$', index),
+)
+##### END URLS
+
+
+if __name__ == '__main__':
+    execute_from_command_line(sys.argv)
